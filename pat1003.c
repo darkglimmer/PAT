6154c18a34af35dfddd3922bc010ed0c84653232
darkglimmer <2392897209@qq.com>
2017-12-17 00:05:03 +0800

    03

--- pat1003.c
+++ pat1003.c
@@ -3,31 +3,28 @@
 int main() {
     int n;
     char s[100];
-    scanf("%d",&n);
-    while(n--){
-        scanf("%s",s);
-        int i=0;
-        int a=0,b=0;
-        int r=0;
-        int pconut=0,tconut=0;
-        while(s[i]){
-            if(s[i]!='P'&&s[i]!='A'&&s[i]!='T') {
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%s", s);
+        int i = 0;
+        int a = 0, b = 0;
+        int r = 0;
+        int pconut = 0, tconut = 0;
+        while (s[i]) {
+            if (s[i] != 'P' && s[i] != 'A' && s[i] != 'T') {
                 r = 1;
-            }
-            else if(s[i]=='P'){
+            } else if (s[i] == 'P') {
                 pconut++;
-                a=i;
-            }
-            else if(s[i]=='T'){
+                a = i;
+            } else if (s[i] == 'T') {
                 tconut++;
-                b=i;
+                b = i;
             }
             i++;
         }
-        if(r==1||pconut!=1||tconut!=1||(b-a)<=1||(b-a)>=4){
+        if (r == 1 || pconut != 1 || tconut != 1 || (b - a) <= 1 || (b - a) >= 4) {
             printf("NO\n");
-        }
-        else {
+        } else {
             printf("YES\n");
         }
     }
